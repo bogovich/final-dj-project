d396e9828bfb17e0b63d0750d5c8ae8faabc670e
bogovich <pkbogovic@gmail.com>
2022-05-05 14:17:37 +0000

    Added choice model.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -125,7 +125,10 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(null=False)
+    is_correct = models.BooleanField(default=False)
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
