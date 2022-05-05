c99fb0393b196bacceb0ac168bc8531733923592
bogovich <pkbogovic@gmail.com>
2022-05-05 14:25:47 +0000

    Registered QuestionAdmin.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -27,10 +27,12 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'grade']
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Lesson, LessonAdmin)
